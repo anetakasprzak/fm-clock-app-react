0c80af02edca5d544961f33e33507d2a5105d399
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-04-19 11:55:10 +0100

    set up the project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import imgDayMobile from "./assets/mobile/bg-image-daytime.jpg";
+import imgNightMobile from "./assets/mobile/bg-image-nighttime.jpg";
+import imgDayTablet from "./assets/tablet/bg-image-daytime.jpg";
+import imgNightTablet from "./assets/tablet/bg-image-nighttime.jpg";
+import imgDayDesktop from "./assets/desktop/bg-image-daytime.jpg";
+import imgNigDesktop from "./assets/desktop/bg-image-nighttime.jpg";
+
+import arrowDown from "./assets/desktop/icon-arrow-down.svg";
+import arrowUp from "./assets/desktop/icon-arrow-up.svg";
+import iconMoon from "./assets/desktop/icon-moon.svg";
+import iconSun from "./assets/desktop/icon-sun.svg";
+import iconRefresh from "./assets/desktop/icon-refresh.svg";
+
+function App() {
+  return <div className="wrapper">CLOCK APP</div>;
+}
+
+export default App;
