a07c4c9c9a21fcfa4ee9330c85c98a7674baadfe
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-04-20 08:59:03 +0100

    btn functionality

--- src/components/ButtonComponent/ButtonComponent.jsx
+++ src/components/ButtonComponent/ButtonComponent.jsx
@@ -1,12 +1,49 @@
 import "./ButtonComponent.css";
-import arrowDown from "../../assets/desktop/icon-arrow-down.svg";
-import arrowUp from "../../assets/desktop/icon-arrow-up.svg";
+import { useState } from "react";
 
 const Button = () => {
+  const [isExpand, setIsExpand] = useState(false);
+
   return (
-    <button className="btn__expand">
-      <p className="btn__expand-text">More</p>
-      <img src={arrowDown} alt="arrow" className="btn__arrow" />
+    <button
+      className="btn__expand"
+      onClick={() => setIsExpand((expand) => !expand)}
+    >
+      <p className="btn__expand-text">{isExpand ? "Less" : "More"}</p>
+
+      {!isExpand ? (
+        <svg
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="btn__arrow"
+        >
+          <circle cx="16" cy="16" r="16" fill="#303030" />
+          <path
+            d="M11.2 13.6001L16 18.4001L20.8 13.6001"
+            stroke="white"
+            strokeWidth="2"
+          />
+        </svg>
+      ) : (
+        <svg
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="btn__arrow"
+        >
+          <circle cx="16" cy="16" r="16" fill="#303030" />
+          <path
+            d="M11.2 18.3999L16 13.5999L20.8 18.3999"
+            stroke="white"
+            stroke-width="2"
+          />
+        </svg>
+      )}
     </button>
   );
 };
