a2106d9210c262695ba5c7232dcd9f39dcb3623a
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-04-22 17:00:53 +0100

    clock css and media queries

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,191 @@
 import { useEffect, useState } from "react";
 import Background from "./components/Background/Background";
-import Button from "./components/ButtonComponent/ButtonComponent";
 import CurrentTimeComponent from "./components/CurrentTimeComponent/CurrentTimeComponent";
 import QuotesComponent from "./components/QuotesComponent/QuotesComponent";
 import Ipbase from "@everapi/ipbase-js";
 
+const mocked = {
+  data: {
+    ip: "82.7.216.59",
+    hostname: null,
+    type: "v4",
+    range_type: {
+      type: "PUBLIC",
+      description: "Public address",
+    },
+    connection: {
+      asn: 5089,
+      organization: "Virgin Media Limited",
+      isp: "Virgin Media Limited",
+      range: "82.7.216.0/24",
+    },
+    location: {
+      geonames_id: 101750609,
+      latitude: 54.68611145019531,
+      longitude: -1.212499976158142,
+      zip: "TS24",
+      continent: {
+        code: "EU",
+        name: "Europe",
+        name_translated: "Europe",
+        geonames_id: 6255148,
+        wikidata_id: "Q46",
+      },
+      country: {
+        alpha2: "GB",
+        alpha3: "GBR",
+        calling_codes: ["+44"],
+        currencies: [
+          {
+            symbol: "£",
+            name: "British Pound Sterling",
+            symbol_native: "£",
+            decimal_digits: 2,
+            rounding: 0,
+            code: "GBP",
+            name_plural: "British pounds sterling",
+            type: "fiat",
+          },
+        ],
+        emoji: "🇬🇧",
+        ioc: "GBR",
+        languages: [
+          {
+            name: "English",
+            name_native: "English",
+          },
+          {
+            name: "Cornish",
+            name_native: "Kernewek",
+          },
+          {
+            name: "Irish",
+            name_native: "Gaeilge",
+          },
+          {
+            name: "Gaelic",
+            name_native: "Gaelic",
+          },
+          {
+            name: "Welsh",
+            name_native: "Cymraeg",
+          },
+        ],
+        name: "United Kingdom",
+        name_translated: "United Kingdom",
+        timezones: ["Europe/London"],
+        is_in_european_union: false,
+        fips: "UK",
+        geonames_id: 85633159,
+        hasc_id: "GB",
+        wikidata_id: "Q145",
+      },
+      city: {
+        fips: null,
+        alpha2: null,
+        geonames_id: 101750609,
+        hasc_id: null,
+        wikidata_id: "Q215752",
+        name: "Hartlepool",
+        name_translated: "Hartlepool",
+      },
+      region: {
+        fips: null,
+        alpha2: null,
+        geonames_id: 1360698621,
+        hasc_id: null,
+        wikidata_id: null,
+        name: "Durham",
+        name_translated: "Durham",
+      },
+    },
+    tlds: [".uk"],
+    timezone: {
+      id: "Europe/London",
+      current_time: "2024-04-20T12:37:32+01:00",
+      code: "BST",
+      is_daylight_saving: true,
+      gmt_offset: 3600,
+    },
+    security: {
+      is_anonymous: null,
+      is_datacenter: null,
+      is_vpn: null,
+      is_bot: null,
+      is_abuser: null,
+      is_known_attacker: null,
+      is_proxy: null,
+      is_spam: null,
+      is_tor: null,
+      proxy_type: null,
+      is_icloud_relay: null,
+      threat_score: null,
+    },
+    domains: {
+      count: null,
+      domains: [],
+    },
+  },
+};
+
+const mocked2 = {
+  abbreviation: "BST",
+  client_ip: "82.7.216.59",
+  datetime: "2024-04-21T08:13:39.416731+01:00",
+  day_of_week: 0,
+  day_of_year: 112,
+  dst: true,
+  dst_from: "2024-03-31T01:00:00+00:00",
+  dst_offset: 3600,
+  dst_until: "2024-10-27T01:00:00+00:00",
+  raw_offset: 0,
+  timezone: "Europe/London",
+  unixtime: 1713683619,
+  utc_datetime: "2024-04-21T07:13:39.416731+00:00",
+  utc_offset: "+01:00",
+  week_number: 16,
+};
+
 function App() {
-  const [apiData, setApiData] = useState({});
+  const [ipApiData, setIpApiData] = useState(mocked);
+  const [worldTimeApiData, setWorldTimeApiData] = useState(mocked2);
 
-  const ipBase = new Ipbase(
-    "ipb_live_qSRYJeK7U7woCu8fcF8zf0jTQy1e5484YSUWExSL"
-  );
+  // const ipBase = new Ipbase(
+  //   "ipb_live_qSRYJeK7U7woCu8fcF8zf0jTQy1e5484YSUWExSL"
+  // );
 
-  useEffect(() => {
-    ipBase.info().then((response) => {
-      setApiData(response);
-    });
-  }, []);
+  // useEffect(() => {
+  //   ipBase.info().then((response) => {
+  //     setIpApiData(response);
+  //   });
+  // }, []);
+
+  // const fetchWorldTimeApi = async (timezoneId) => {
+  //   try {
+  //     const res = await fetch(
+  //       `http://worldtimeapi.org/api/timezone/${timezoneId}`
+  //     );
+  //     const data = await res.json();
+  //     setWorldTimeApiData(data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   if (ipApiData) {
+  //     fetchWorldTimeApi(ipApiData?.data?.timezone?.id);
+  //   }
+  // }, [ipApiData]);
 
   return (
     <div className="wrapper">
       <Background>
         <QuotesComponent />
-        <CurrentTimeComponent apiData={apiData} />
-        <Button />
+        <CurrentTimeComponent
+          ipApiData={ipApiData}
+          worldTimeApiData={worldTimeApiData}
+        />
       </Background>
     </div>
   );
--- src/components/CurrentTimeComponent/CurrentTimeComponent.jsx
+++ src/components/CurrentTimeComponent/CurrentTimeComponent.jsx
@@ -1,18 +1,54 @@
 import "./CurrentTimeComponent.css";
+import Button from "../ButtonComponent/ButtonComponent";
 import iconMoon from "../../assets/desktop/icon-moon.svg";
 import iconSun from "../../assets/desktop/icon-sun.svg";
 
-const CurrentTimeComponent = ({ apiData }) => {
-  const cityName = apiData?.data?.location?.city?.name;
-  const countryName = apiData?.data?.location?.country?.name;
-  const timezoneCode = apiData?.data?.timezone?.code;
-  const currentTime = apiData?.data?.timezone?.current_time;
+const CurrentTimeComponent = ({ ipApiData }) => {
+  const cityName = ipApiData?.data?.location?.city?.name;
+  const countryName = ipApiData?.data?.location?.country?.name;
+  const timezoneCode = ipApiData?.data?.timezone?.code;
+  const currentTime = ipApiData?.data?.timezone?.current_time;
 
   const hour = new Date(currentTime).getHours();
   const minutes = new Date(currentTime).getMinutes();
-  console.log(`${hour}:${minutes}`);
 
-  return <div className="">{"name"}</div>;
+  const dayTime = currentTime > 5 && currentTime < 18;
+  const morning = currentTime > 5 && currentTime < 12;
+  const afternoon = currentTime > 12 && currentTime < 18;
+
+  return (
+    <div className="time__btn--wrapper">
+      <div className="current__time--box">
+        <div className="greeting__box">
+          {dayTime ? (
+            <img src={iconSun} alt="icon sun" className="current__time--icon" />
+          ) : (
+            <img
+              src={iconMoon}
+              alt="icon moon"
+              className="current__time--icon"
+            />
+          )}
+          <p className="greeting__text">
+            {morning && !afternoon && "Good morning"}
+            {afternoon && "Good afternoon"}
+            {!morning && !afternoon && "Good evening"}
+          </p>
+          <span className="greeting__currently greeting__text">
+            , it&apos;s currently
+          </span>
+        </div>
+        <div className="current__hour">
+          {hour}:{minutes}
+          <span className="current__hour--code"> {timezoneCode}</span>
+        </div>
+        <p className="location">
+          in {cityName}, {countryName}
+        </p>
+      </div>
+      <Button />
+    </div>
+  );
 };
 
 export default CurrentTimeComponent;
